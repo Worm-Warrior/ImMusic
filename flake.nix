61458b0e7f1b9ef51a6d0393aa2a4c78bb4970f0
Harrison Voss <hvoss@iastate.edu>
2026-03-18 19:13:22 -0500

    nixos flake added

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          # Build tools
+          cmake
+          gnumake
+          gcc
+          pkg-config
+
+          # libs
+          sdl3
+          pkgs.ffmpeg
+          taglib_1
+        ];
+
+        # Optional: set any env vars your project needs
+        shellHook = ''
+          echo "C++ dev environment ready"
+        '';
+      };
+    };
+}
